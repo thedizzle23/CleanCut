490b2bf0dcb92290d55dd584cad3953c986da2dc
Robert DeNicola <thedizzle23@gmail.com>
2012-06-17 10:19:34 -0400

    commit one

---
+++ MultipleViewsBase/MultipleViewsBaseTests/MultipleViewsBaseTests.h
@@ -0,0 +1,13 @@
+//
+//  MultipleViewsBaseTests.h
+//  MultipleViewsBaseTests
+//
+//  Created by Robert DeNicola on 6/17/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MultipleViewsBaseTests : SenTestCase
+
+@end
---
+++ MultipleViewsBase/MultipleViewsBaseTests/MultipleViewsBaseTests.m
@@ -0,0 +1,32 @@
+//
+//  MultipleViewsBaseTests.m
+//  MultipleViewsBaseTests
+//
+//  Created by Robert DeNicola on 6/17/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "MultipleViewsBaseTests.h"
+
+@implementation MultipleViewsBaseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MultipleViewsBaseTests");
+}
+
+@end
